9f56e65587a3db2aa1211d098b5723a34e151128
AkimioJR <akimio.jr@gmail.com>
2026-03-28 09:49:34 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "BBCode",
+  platforms: [
+    .macOS(.v14),
+    .iOS(.v17),
+    .tvOS(.v17),
+    .watchOS(.v10),
+    .visionOS(.v1),
+  ],
+  products: [
+    .library(name: "BBCodeParser", targets: ["BBCodeParser"])
+
+  ],
+  dependencies: [
+
+  ],
+  targets: [
+    .target(
+      name: "BBCodeParser",
+      dependencies: []
+    ),
+  ]
+)
