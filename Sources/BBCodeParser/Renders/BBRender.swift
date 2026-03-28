dcadab124f71c0b0638524cf11d19c0c5a977598
AkimioJR <akimio.jr@gmail.com>
2026-03-28 15:05:40 +0800

    feat: 添加 BBCode 渲染功能，支持使用解析器和标签管理器

--- Sources/BBCodeParser/Renders/BBRender.swift
+++ Sources/BBCodeParser/Renders/BBRender.swift
@@ -1,1 +1,15 @@
 public typealias BBRender<K: Hashable, V, R> = @Sendable (BBNode, [K: V]) -> R
+
+extension BBCode {
+    public func render<K: Hashable, V, R>(
+        _ bbcode: String,
+        using parser: BBParser = defaultBBParser,
+        tagManager: BBTagManager = BBTagManager(),
+        renderer: BBRender<K, V, R>,
+        args: [K: V] = [:],
+    ) throws(BBCodeError) -> R {
+        let domTree = try parser(bbcode, BBParserContext(tagManager: tagManager))
+        handleNewlineAndParagraph(node: domTree)
+        return renderer(domTree, args)
+    }
+}
